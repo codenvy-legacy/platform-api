5a9c7283740a97da4c9bcbd9182c3dbe28574d60
Eugene Voeovodin <evoevodin@codenvy.com>
2013-12-05 18:28:51 +0200

    IDEX-249: Added possibility to remove files that was not modified in some period of time.

--- codenvy-api-builder/src/main/java/com/codenvy/api/builder/internal/SourcesManagerImpl.java
+++ codenvy-api-builder/src/main/java/com/codenvy/api/builder/internal/SourcesManagerImpl.java
@@ -27,6 +27,7 @@ import com.google.common.hash.Hashing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -34,13 +35,19 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Implementation of SourcesManager that stores sources locally and gets only updated files over virtual file system RESt API.
@@ -48,14 +55,31 @@ import java.util.concurrent.FutureTask;
  * @author <a href="mailto:aparfonov@codenvy.com">Andrey Parfonov</a>
  */
 public class SourcesManagerImpl implements DownloadPlugin, SourcesManager {
-    private static final Logger LOG = LoggerFactory.getLogger(SourcesManagerImpl.class);
+    private static final Logger   LOG                        = LoggerFactory.getLogger(SourcesManagerImpl.class);
+    private static final String   SCHEDULER_TASK_KEY         = "scheduler_check_task_key";
+    /**
+     * Time of files life estimating settings.
+     * For example: if ESTIMATE_OF_FILE_LIFE_UNIT is TimeUnit.DAYS
+     * and ESTIMATE_OF_FILE_LIFE is 1, then all files that was not modified in 1d
+     * will be deleted by next scheduler wave.
+     */
+    private static final TimeUnit ESTIMATE_OF_FILE_LIFE_UNIT = TimeUnit.DAYS;
+    private static final long     ESTIMATE_OF_FILE_LIFE      = 2;
+    /** Scheduler tasks executing settings */
+    private static final TimeUnit TASK_EXECUTION_PERIOD_UNIT = TimeUnit.HOURS;
+    private static final long     TASK_EXECUTION_PERIOD      = 2;
 
     private final java.io.File                        directory;
+    private final ScheduledExecutorService            checkAndDeleteFilesScheduler;
     private final ConcurrentMap<String, Future<Void>> tasks;
 
     public SourcesManagerImpl(java.io.File directory) {
         this.directory = directory;
         tasks = new ConcurrentHashMap<>();
+        checkAndDeleteFilesScheduler = Executors.newSingleThreadScheduledExecutor();
+        checkAndDeleteFilesScheduler
+                .scheduleAtFixedRate(createRemoveFilesWithRemainingLifeTimeTask(), TASK_EXECUTION_PERIOD, TASK_EXECUTION_PERIOD,
+                                     TASK_EXECUTION_PERIOD_UNIT);
     }
 
     @Override
@@ -192,5 +216,88 @@ public class SourcesManagerImpl implements DownloadPlugin, SourcesManager {
     @Override
     public java.io.File getDirectory() {
         return directory;
+    }
+
+
+    /**
+     * Create runnable task that will check last files modifications and remove any of them
+     * if it needed.
+     *
+     * @return runnable task for
+     */
+    private Runnable createRemoveFilesWithRemainingLifeTimeTask() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Future<Void> future = tasks.get(SCHEDULER_TASK_KEY);
+                if (future == null) {
+                    FutureTask<Void> newFuture = new FutureTask<>(new Runnable() {
+                        @Override
+                        public void run() {
+                            removeFilesWithRemainingLifeTime(directory);
+                        }
+                    }, null);
+                    future = tasks.putIfAbsent(SCHEDULER_TASK_KEY, newFuture);
+                    if (future == null) {
+                        future = newFuture;
+                        newFuture.run();
+                    }
+                }
+                try {
+                    //block current thread until files checking is not complete.
+                    future.get();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } catch (ExecutionException e) {
+                    // Runnable does not throw checked exceptions.
+                    final Throwable cause = e.getCause();
+                    if (cause instanceof Error) {
+                        throw (Error)cause;
+                    } else {
+                        throw (RuntimeException)cause;
+                    }
+                } finally {
+                    tasks.remove(SCHEDULER_TASK_KEY);
+                }
+            }
+        };
+    }
+
+
+    /**
+     * Remove all files with remaining life time
+     *
+     * @param rootDirectory
+     *         directory that contains files that should be checked
+     */
+    private void removeFilesWithRemainingLifeTime(File rootDirectory) {
+        Queue<File> shouldBeChecked = new LinkedList<>();
+        shouldBeChecked.addAll(Arrays.asList(rootDirectory.listFiles()));
+        while (shouldBeChecked.size() != 0) {
+            File current = shouldBeChecked.poll();
+            if (isFileShouldBeRemoved(current)) {
+                current.delete();
+            } else if (current.isDirectory()) {
+                shouldBeChecked.addAll(Arrays.asList(current.listFiles()));
+            }
+        }
+    }
+
+    /**
+     * Check file remaining estimate
+     *
+     * @param file
+     *         file that will be checked
+     * @return <code>true</code> if file has last modification time more than time that was estimated
+     */
+    private boolean isFileShouldBeRemoved(File file) {
+        try {
+            long lastModifiedTimeInMilliseconds = Files.getLastModifiedTime(file.toPath()).toMillis();
+            return ESTIMATE_OF_FILE_LIFE_UNIT.convert(System.currentTimeMillis() - lastModifiedTimeInMilliseconds,
+                                                      TimeUnit.MILLISECONDS) >= ESTIMATE_OF_FILE_LIFE;
+        } catch (IOException e) {
+            LOG.error(String.format("It is not possible to get last modification time of %s", file.getAbsolutePath()), e);
+            return false;
+        }
     }
 }

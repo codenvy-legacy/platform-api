42dbfa66697ed6338246d43b70f36a4abde91eba
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2013-12-18 16:06:51 +0200

    CLDIDE-1415: Implement parametrized factories

--- codenvy-api-factory/src/main/java/com/codenvy/api/factory/Variable.java
+++ codenvy-api-factory/src/main/java/com/codenvy/api/factory/Variable.java
@@ -32,6 +32,26 @@ public class Variable {
         this.entries = entries;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Variable variable = (Variable)o;
+
+        if (entries != null ? !entries.equals(variable.entries) : variable.entries != null) return false;
+        if (files != null ? !files.equals(variable.files) : variable.files != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = files != null ? files.hashCode() : 0;
+        result = 31 * result + (entries != null ? entries.hashCode() : 0);
+        return result;
+    }
+
     public static class Replacement {
         private String find;
         private String replace;
@@ -58,6 +78,26 @@ public class Variable {
 
         public void setReplace(String replace) {
             this.replace = replace;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Replacement that = (Replacement)o;
+
+            if (find != null ? !find.equals(that.find) : that.find != null) return false;
+            if (replace != null ? !replace.equals(that.replace) : that.replace != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = find != null ? find.hashCode() : 0;
+            result = 31 * result + (replace != null ? replace.hashCode() : 0);
+            return result;
         }
     }
 }

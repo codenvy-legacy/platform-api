d493ba756ce73d96d7c91ea8cf2195622156d5dd
Max Shaposhnik <mshaposhnik@codenvy.com>
2014-02-19 14:03:01 +0200

    war module add;

---
+++ codenvy-api-organization-mongo/src/main/java/com/codenvy/api/organization/mongo/deploy/DummyOrganizationServletModule.java
@@ -0,0 +1,34 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * __________________
+ *
+ *  [2012] - [2013] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.api.organization.mongo.deploy;
+
+
+import com.codenvy.api.organization.mongo.filter.filter.DummyRequestFilter;
+import com.codenvy.inject.DynaModule;
+import com.google.inject.servlet.ServletModule;
+
+import org.everrest.guice.servlet.GuiceEverrestServlet;
+
+@DynaModule
+public class DummyOrganizationServletModule extends ServletModule {
+    @Override
+    protected void configureServlets() {
+        filter("/*").through(DummyRequestFilter.class);
+        serve("/*").with(GuiceEverrestServlet.class);
+    }
+}

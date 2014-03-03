34c152d033d1ac8dfa9f503c1cb2f67c70936d3c
Alex Reshetnyak <areshetnyak@codenvy.com>
2014-03-03 13:10:03 +0200

    DASHB-333 : Add field name to MetricValueDTO.

--- codenvy-api-analytics/src/main/java/com/codenvy/api/analytics/impl/DummyMetricHandler.java
+++ codenvy-api-analytics/src/main/java/com/codenvy/api/analytics/impl/DummyMetricHandler.java
@@ -64,6 +64,7 @@ public class DummyMetricHandler implements MetricHandler {
 
     private MetricValueDTO createDummyMetricValueDTO(String metricName) {
         MetricValueDTO metricValueDTO = DtoFactory.getInstance().createDto(MetricValueDTO.class);
+        metricValueDTO.setName(metricName);
         if ("FACTORY_USED".equalsIgnoreCase(metricName)) {
             metricValueDTO.setValue(String.valueOf(new Random().nextInt(256)));
         } else {

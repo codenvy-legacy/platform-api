8261e4f0cd33d25e8e33b901ba972c87eb796b85
Andrey Parfonov <aparfonov@codenvy.com>
2014-04-28 14:46:09 +0300

    IDEX-570: add line number to each logged message

--- codenvy-api-runner/src/main/java/com/codenvy/api/runner/internal/RunnerEvent.java
+++ codenvy-api-runner/src/main/java/com/codenvy/api/runner/internal/RunnerEvent.java
@@ -54,6 +54,47 @@ public class RunnerEvent {
         }
     }
 
+    /*
+    Seems we can't guaranty correct order of messages on the client (browser) side, that means we need to wrap each line with simple object
+    that keeps line's number.
+     */
+    public static class LoggedMessage {
+        private String message;
+        private int    lineNum;
+
+        public LoggedMessage(String message, int lineNum) {
+            this.message = message;
+            this.lineNum = lineNum;
+        }
+
+        public LoggedMessage() {
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getLineNum() {
+            return lineNum;
+        }
+
+        public void setLineNum(int lineNum) {
+            this.lineNum = lineNum;
+        }
+
+        @Override
+        public String toString() {
+            return "LoggedMessage{" +
+                   "message='" + message + '\'' +
+                   ", lineNum=" + lineNum +
+                   '}';
+        }
+    }
+
     public static RunnerEvent startedEvent(long processId, String workspace, String project) {
         return new RunnerEvent(EventType.STARTED, processId, workspace, project);
     }
@@ -66,23 +107,25 @@ public class RunnerEvent {
         return new RunnerEvent(EventType.ERROR, processId, workspace, project, message);
     }
 
-    public static RunnerEvent messageLoggedEvent(long processId, String workspace, String project, String message) {
+    public static RunnerEvent messageLoggedEvent(long processId, String workspace, String project, LoggedMessage message) {
         return new RunnerEvent(EventType.MESSAGE_LOGGED, processId, workspace, project, message);
     }
 
     /** Event type. */
-    private EventType type;
+    private EventType     type;
     /** Id of application process that produces the event. */
-    private long      processId;
+    private long          processId;
     /** Id of workspace that produces the event. */
-    private String    workspace;
+    private String        workspace;
     /** Name of project that produces the event. */
-    private String    project;
+    private String        project;
+    /** Error message. */
+    private String        error;
     /** Message associated with this event. Makes sense only for {@link EventType#MESSAGE_LOGGED} or {@link EventType#ERROR} events. */
-    private String    message;
+    private LoggedMessage message;
 
 
-    RunnerEvent(EventType type, long processId, String workspace, String project, String message) {
+    RunnerEvent(EventType type, long processId, String workspace, String project, LoggedMessage message) {
         this.type = type;
         this.processId = processId;
         this.workspace = workspace;
@@ -90,8 +133,19 @@ public class RunnerEvent {
         this.message = message;
     }
 
-    RunnerEvent(EventType type, long taskId, String workspace, String project) {
-        this(type, taskId, workspace, project, null);
+    RunnerEvent(EventType type, long processId, String workspace, String project, String error) {
+        this.type = type;
+        this.processId = processId;
+        this.workspace = workspace;
+        this.project = project;
+        this.error = error;
+    }
+
+    RunnerEvent(EventType type, long processId, String workspace, String project) {
+        this.type = type;
+        this.processId = processId;
+        this.workspace = workspace;
+        this.project = project;
     }
 
     public RunnerEvent() {
@@ -129,12 +183,20 @@ public class RunnerEvent {
         this.project = project;
     }
 
-    public String getMessage() {
+    public LoggedMessage getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
+    public void setMessage(LoggedMessage message) {
         this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 
     @Override
@@ -145,6 +207,7 @@ public class RunnerEvent {
                ", workspace='" + workspace + '\'' +
                ", project='" + project + '\'' +
                ", message='" + message + '\'' +
+               ", error='" + error + '\'' +
                '}';
     }
 }

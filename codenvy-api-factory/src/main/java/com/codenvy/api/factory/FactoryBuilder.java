ad87c28833b25c5d389769c1c62a6d6cbc2bfb50
Sergey Kabashnyuk <skabashnyuk@codenvy.com>
2014-05-29 12:07:03 +0300

    Rewert formating

--- codenvy-api-factory/src/main/java/com/codenvy/api/factory/FactoryBuilder.java
+++ codenvy-api-factory/src/main/java/com/codenvy/api/factory/FactoryBuilder.java
@@ -53,28 +53,19 @@ public class FactoryBuilder extends NonEncodedFactoryBuilder {
     private static final Logger LOG = LoggerFactory.getLogger(FactoryService.class);
 
     private static final String INVALID_PARAMETER_MESSAGE                      =
-            "Passed in an invalid parameter.  You either provided a non-valid parameter, or that parameter is not accepted for this " +
-            "Factory version.  For more information, please visit http://docs.codenvy" +
-            ".com/user/creating-factories/factory-parameter-reference/.";
+            "Passed in an invalid parameter.  You either provided a non-valid parameter, or that parameter is not accepted for this Factory version.  For more information, please visit http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
     private static final String INVALID_VERSION_MESSAGE                        =
-            "You have provided an inaccurate or deprecated Factory Version.  For more information, " +
-            "please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
+            "You have provided an inaccurate or deprecated Factory Version.  For more information, please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
     private static final String MISSING_MANDATORY_MESSAGE                      =
-            "You are missing a mandatory parameter.  For more information, please visit: http://docs.codenvy" +
-            ".com/user/creating-factories/factory-parameter-reference/.";
+            "You are missing a mandatory parameter.  For more information, please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
     private static final String PARAMETRIZED_INVALID_TRACKED_PARAMETER_MESSAGE =
-            "You have provided a Tracked Factory parameter %s, and you do not have a valid orgId %s.  You could have provided the wrong " +
-            "code, your subscription has expired, or you do not have a valid subscription account.  Please contact info@codenvy.com with " +
-            "any questions.";
+            "You have provided a Tracked Factory parameter %s, and you do not have a valid orgId %s.  You could have provided the wrong code, your subscription has expired, or you do not have a valid subscription account.  Please contact info@codenvy.com with any questions.";
     private static final String PARAMETRIZED_INVALID_PARAMETER_MESSAGE         =
-            "You have provided an invalid parameter %s for this version of Factory parameters %s.  For more information, " +
-            "please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
+            "You have provided an invalid parameter %s for this version of Factory parameters %s.  For more information, please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
     private static final String PARAMETRIZED_ENCODED_ONLY_PARAMETER_MESSAGE    =
-            "You submitted a parameter that can only be submitted through an encoded Factory URL %s.  For more information, " +
-            "please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
+            "You submitted a parameter that can only be submitted through an encoded Factory URL %s.  For more information, please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
     private static final String PARAMETRIZED_ILLEGAL_PARAMETER_VALUE_MESSAGE   =
-            "The parameter %s has a value submitted %s with a value that is unexpected. For more information, " +
-            "please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
+            "The parameter %s has a value submitted %s with a value that is unexpected. For more information, please visit: http://docs.codenvy.com/user/creating-factories/factory-parameter-reference/.";
 
     /** List contains all possible implementation of factory legacy converters. */
     static final List<LegacyConverter> LEGACY_CONVERTERS;

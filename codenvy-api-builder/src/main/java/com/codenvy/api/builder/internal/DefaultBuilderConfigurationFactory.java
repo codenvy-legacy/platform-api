aa7fc4828551aa29624dd0d753223091e532758a
Florent Benoit <Florent.Benoit@serli.com>
2014-06-19 14:13:55 +0200

    IDEX-776 : Keep the original folder name in the name of the generated folder so that it is compliant with legacy projects using the folder name through classpath

--- codenvy-api-builder/src/main/java/com/codenvy/api/builder/internal/DefaultBuilderConfigurationFactory.java
+++ codenvy-api-builder/src/main/java/com/codenvy/api/builder/internal/DefaultBuilderConfigurationFactory.java
@@ -14,7 +14,9 @@ import com.codenvy.api.builder.BuilderException;
 import com.codenvy.api.builder.dto.BaseBuilderRequest;
 import com.codenvy.api.builder.dto.BuildRequest;
 import com.codenvy.api.builder.dto.DependencyRequest;
+import com.codenvy.api.project.shared.dto.ProjectDescriptor;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
@@ -58,11 +60,30 @@ public class DefaultBuilderConfigurationFactory implements BuilderConfigurationF
         throw new BuilderException("Unsupported type of request");
     }
 
+    /**
+     * Work directory that will be created will match build-<generated number>/project-name .
+     * @param request the request for this new build
+     * @return the folder that will be used as work directory
+     * @throws BuilderException if there is any exception (like creating the directories)
+     */
     protected java.io.File createWorkDir(BaseBuilderRequest request) throws BuilderException {
+
+        // Get project name through project descriptor
+        String projectName = request.getProjectDescriptor().getName();
+
+        // Create a temp directory
+        File tempDirectory;
         try {
-            return Files.createTempDirectory(builder.getBuildDirectory().toPath(), "build-").toFile();
+            tempDirectory = Files.createTempDirectory(builder.getBuildDirectory().toPath(), "build-").toFile();
         } catch (IOException e) {
             throw new BuilderException(e);
         }
+        // Now create subdirectory to match the project name. If project is relying on its name it is kept.
+        try {
+            return Files.createDirectory(new File(tempDirectory, projectName).toPath()).toFile();
+        } catch (IOException e) {
+            throw new BuilderException(e);
+        }
+
     }
 }

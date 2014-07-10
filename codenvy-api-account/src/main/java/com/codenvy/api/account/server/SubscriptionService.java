fa799b97172e4e36de4a25060b2c81aadfd3dfcd
Eugene Voevodin <evoevodin@codenvy.com>
2014-07-10 13:02:35 +0300

    Added javadoc

--- codenvy-api-account/src/main/java/com/codenvy/api/account/server/SubscriptionService.java
+++ codenvy-api-account/src/main/java/com/codenvy/api/account/server/SubscriptionService.java
@@ -24,27 +24,92 @@ public abstract class SubscriptionService {
     private final String serviceId;
     private final String displayName;
 
+    /**
+     * @param serviceId
+     *         service identifier
+     * @param displayName
+     *         service name
+     */
     public SubscriptionService(String serviceId, String displayName) {
         this.serviceId = serviceId;
         this.displayName = displayName;
     }
 
+    /**
+     * Should be invoked before subscription creation. It can change subscription attributes or fields
+     * to prepare subscription for creating
+     *
+     * @param subscription
+     *         subscription to prepare
+     * @throws ApiException
+     *         when subscription is incompatible with system
+     */
     public abstract void beforeCreateSubscription(Subscription subscription) throws ApiException;
 
+    /**
+     * Should be invoked after subscription creation
+     *
+     * @param subscription
+     *         created subscription
+     * @throws ApiException
+     *         when some error occurs while processing {@code subscription}
+     */
     public abstract void afterCreateSubscription(Subscription subscription) throws ApiException;
 
+    /**
+     * Should be invoked after subscription removing
+     *
+     * @param subscription
+     *         subscription that was removed
+     * @throws ApiException
+     *         when some error occurs while processing {@code subscription}
+     */
     public abstract void onRemoveSubscription(Subscription subscription) throws ApiException;
 
+    /**
+     * Should be invoked to check subscription.
+     * The one of use cases is use this method to check subscription expiration etc
+     *
+     * @param subscription
+     *         subscription that need to be checked
+     * @throws ApiException
+     *         when some error occurs while checking {@code subscription}
+     */
     public abstract void onCheckSubscription(Subscription subscription) throws ApiException;
 
+    /**
+     * Should be invoked after subscription update
+     *
+     * @param oldSubscription
+     *         subscription before update
+     * @param newSubscription
+     *         subscription after update
+     * @throws ApiException
+     *         when some error occurs while processing {@code subscription}
+     */
     public abstract void onUpdateSubscription(Subscription oldSubscription, Subscription newSubscription) throws ApiException;
 
+    /**
+     * Generates service amount
+     *
+     * @param subscription
+     *         subscription that going to be analyzed for getting amount
+     * @return service amount
+     * @throws ApiException
+     *         when it is not possible to generate amount
+     */
     public abstract double tarifficate(Subscription subscription) throws ApiException;
 
+    /**
+     * @return service identifier
+     */
     public String getServiceId() {
         return serviceId;
     }
 
+    /**
+     * @return service name
+     */
     public String getDisplayName() {
         return displayName;
     }

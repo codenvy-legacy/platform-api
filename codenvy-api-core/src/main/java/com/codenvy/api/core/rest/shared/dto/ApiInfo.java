c513d37a8c3629000a9bac372e9abf3a4f0b0887
Andrey Parfonov <aparfonov@codenvy.com>
2014-08-08 10:55:23 +0300

    IDEX-999

---
+++ codenvy-api-core/src/main/java/com/codenvy/api/core/rest/shared/dto/ApiInfo.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package com.codenvy.api.core.rest.shared.dto;
+
+import com.codenvy.dto.shared.DTO;
+
+/**
+ * @author andrew00x
+ */
+@DTO
+public interface ApiInfo {
+    String getSpecificationVendor();
+
+    ApiInfo withSpecificationVendor(String specificationVendor);
+
+    void setSpecificationVendor(String specificationVendor);
+
+    String getImplementationVendor();
+
+    ApiInfo withImplementationVendor(String implementationVendor);
+
+    void setImplementationVendor(String implementationVendor);
+
+    String getSpecificationTitle();
+
+    ApiInfo withSpecificationTitle(String specificationTitle);
+
+    void setSpecificationTitle(String specificationTitle);
+
+    String getSpecificationVersion();
+
+    ApiInfo withSpecificationVersion(String specificationVersion);
+
+    void setSpecificationVersion(String specificationVersion);
+
+    String getImplementationVersion();
+
+    ApiInfo withImplementationVersion(String implementationVersion);
+
+    void setImplementationVersion(String implementationVersion);
+
+    String getScmRevision();
+
+    ApiInfo withScmRevision(String scmRevision);
+
+    void setScmRevision(String scmRevision);
+}

559a72f0ccf26648a6c9f1e2f181e0da9a670ff4
Andrey Parfonov <aparfonov@codenvy.com>
2014-09-19 16:56:32 +0300

    Add mechanism for searching Link by its relation

--- codenvy-api-core/src/main/java/com/codenvy/api/core/rest/shared/Links.java
+++ codenvy-api-core/src/main/java/com/codenvy/api/core/rest/shared/Links.java
@@ -10,8 +10,12 @@
  *******************************************************************************/
 package com.codenvy.api.core.rest.shared;
 
+import com.codenvy.api.core.rest.shared.dto.Hyperlinks;
 import com.codenvy.api.core.rest.shared.dto.Link;
+import com.codenvy.api.core.rest.shared.dto.LinkParameter;
+import com.codenvy.dto.server.DtoFactory;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -21,7 +25,7 @@ import java.util.List;
  */
 public class Links {
     /**
-     * Find link by its relation in the specified list.
+     * Find first link in the specified list by its relation.
      *
      * @param rel
      *         link's relation
@@ -36,6 +40,69 @@ public class Links {
             }
         }
         return null;
+    }
+
+    /**
+     * Find all links in the specified list by its relation.
+     *
+     * @param rel
+     *         link's relation
+     * @param links
+     *         list of links
+     * @return found link or {@code null}
+     */
+    public static List<Link> getLinks(String rel, List<Link> links) {
+        final List<Link> result = new LinkedList<>();
+        for (Link link : links) {
+            if (rel.equals(link.getRel())) {
+                result.add(link);
+            }
+        }
+        return result;
+    }
+
+    public static Link getLink(Hyperlinks links, String rel) {
+        return getLink(rel, links.getLinks());
+    }
+
+    public static List<Link> getLinks(Hyperlinks links, String rel) {
+        return getLinks(rel, links.getLinks());
+    }
+
+    public static Link createLink(String method, String href, String consumes, String produces, String rel, LinkParameter... params) {
+        List<LinkParameter> l = null;
+        if (params != null && params.length > 0) {
+            l = new LinkedList<>();
+            java.util.Collections.addAll(l, params);
+        }
+        return createLink(method, href, consumes, produces, rel, l);
+    }
+
+    public static Link createLink(String method, String href, String consumes, String produces, String rel, List<LinkParameter> params) {
+        return DtoFactory.getInstance().createDto(Link.class)
+                         .withMethod(method)
+                         .withHref(href)
+                         .withConsumes(consumes)
+                         .withProduces(produces)
+                         .withRel(rel)
+                         .withParameters(params);
+    }
+
+    public static Link createLink(String method, String href, String consumes, String produces, String rel) {
+        return DtoFactory.getInstance().createDto(Link.class)
+                         .withMethod(method)
+                         .withHref(href)
+                         .withConsumes(consumes)
+                         .withProduces(produces)
+                         .withRel(rel);
+    }
+
+    public static Link createLink(String method, String href, String produces, String rel) {
+        return DtoFactory.getInstance().createDto(Link.class).withMethod(method).withHref(href).withProduces(produces).withRel(rel);
+    }
+
+    public static Link createLink(String method, String href, String rel) {
+        return DtoFactory.getInstance().createDto(Link.class).withMethod(method).withHref(href).withRel(rel);
     }
 
     private Links() {

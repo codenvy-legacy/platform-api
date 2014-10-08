6da91935b2711730a2e228da133c0cc021422d9d
Alexander Garagatyi <agaragatyi@codenvy.com>
2014-10-08 14:32:29 +0300

    add fix

--- codenvy-api-factory/src/main/java/com/codenvy/api/factory/FactoryUrlBaseValidator.java
+++ codenvy-api-factory/src/main/java/com/codenvy/api/factory/FactoryUrlBaseValidator.java
@@ -287,12 +287,15 @@ public abstract class FactoryUrlBaseValidator {
             }
         }
 
-        if (validSince <= validUntil) {
-            throw new ConflictException("Valid until date should be greater than valid since.");
-        }
-
-        if (new Date().after(new Date(validUntil))) {
-            throw new ConflictException("Valid until date can't be less than current date.");
+        if (validSince != 0) {
+            if (validUntil != 0) {
+                if (validSince <= validUntil) {
+                    throw new ConflictException("Valid until date should be greater than valid since.");
+                }
+                if (new Date().after(new Date(validUntil))) {
+                    throw new ConflictException("Valid until date can't be less than current date.");
+                }
+            }
         }
     }
 }

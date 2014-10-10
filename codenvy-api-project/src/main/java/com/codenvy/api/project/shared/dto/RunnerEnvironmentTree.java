cff3d0dd3a7da24c79722a0a49f9bfd906831e7d
Andrey Parfonov <aparfonov@codenvy.com>
2014-10-10 12:10:10 +0300

    Rework RunnerEnvironmentTree according to GWT tree needs

--- codenvy-api-project/src/main/java/com/codenvy/api/project/shared/dto/RunnerEnvironmentTree.java
+++ codenvy-api-project/src/main/java/com/codenvy/api/project/shared/dto/RunnerEnvironmentTree.java
@@ -13,6 +13,7 @@ package com.codenvy.api.project.shared.dto;
 import com.codenvy.dto.shared.DTO;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
 
 /**
@@ -22,25 +23,66 @@ import java.util.List;
  */
 @DTO
 public interface RunnerEnvironmentTree {
+    /**
+     * Gets runner environment on current tree level. If this method returns {@code null} that means there is no any runner environments on
+     * current level. Always need check {@link #getChildren()} for child environments.
+     * <pre>
+     *     + Java
+     *       |- Web
+     *         |- Spring
+     *           |- Tomcat7
+     *           |- JBoss7
+     *           |- Jetty9
+     *         |- Struts
+     *           |- ...
+     *     + Python
+     *       |- Web
+     *         |- ...
+     *     + Ruby
+     *       |- Web
+     *         |- Rails
+     *           |- ...
+     * </pre>
+     * In example above there is no any environment on level Java, Java/Web.
+     */
+    @Nullable
+    RunnerEnvironment getEnvironment();
+
+    /**
+     * Gets runner environment on current tree level.
+     *
+     * @see #getEnvironment()
+     */
+    void setEnvironment(RunnerEnvironment environment);
+
+    RunnerEnvironmentTree withEnvironment(RunnerEnvironment environment);
+
+    /** Gets category. Need this for display tree on client side if there is no runner environment on current level. */
     @Nonnull
-    String getSubCategory();
+    String getCategory();
 
-    void setSubCategory(@Nonnull String subCategory);
+    /**
+     * Sets category.
+     *
+     * @see #getCategory()
+     */
+    void setCategory(@Nonnull String category);
 
-    RunnerEnvironmentTree withSubCategory(@Nonnull String subCategory);
+    RunnerEnvironmentTree withCategory(@Nonnull String category);
 
+    /**
+     * Gets child environments. Empty list means that current tree level is last in hierarchy and method {@link #getEnvironment()} must not
+     * return {@code null}.
+     */
     @Nonnull
     List<RunnerEnvironmentTree> getChildren();
 
+    /**
+     * Sets child environments.
+     *
+     * @see #getChildren()
+     */
     void setChildren(List<RunnerEnvironmentTree> children);
 
     RunnerEnvironmentTree withChildren(List<RunnerEnvironmentTree> children);
-
-    /** Gets RunnerEnvironments of current tree level. */
-    @Nonnull
-    List<RunnerEnvironment> getEnvironments();
-
-    void setEnvironments(List<RunnerEnvironment> environments);
-
-    RunnerEnvironmentTree withEnvironments(List<RunnerEnvironment> environments);
 }

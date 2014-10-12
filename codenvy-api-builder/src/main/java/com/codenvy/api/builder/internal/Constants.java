ab5a480d023809de3b1ce11c61fbd821090a7f83
Andrey Parfonov <aparfonov@codenvy.com>
2014-10-12 16:17:25 +0300

    remove deprecated constants

--- codenvy-api-builder/src/main/java/com/codenvy/api/builder/internal/Constants.java
+++ codenvy-api-builder/src/main/java/com/codenvy/api/builder/internal/Constants.java
@@ -71,10 +71,6 @@ public class Constants {
     public static final String CLEANUP_RESULT_TIME = KEEP_RESULT_TIME;
     /** @deprecated use {@link #QUEUE_SIZE} */
     public static final String INTERNAL_QUEUE_SIZE = QUEUE_SIZE;
-    /** @deprecated user {@link #WAITING_TIME} */
-    public static final String MAX_TIME_IN_QUEUE   = WAITING_TIME;
-    /** @deprecated use {@link #MAX_EXECUTION_TIME} */
-    public static final String BUILD_TIMEOUT       = MAX_EXECUTION_TIME;
 
     // attributes of workspace which are interested for builder
     public static final String BUILDER_EXECUTION_TIME = "codenvy:builder_execution_time";

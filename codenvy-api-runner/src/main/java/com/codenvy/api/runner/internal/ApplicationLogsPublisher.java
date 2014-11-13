4fab0b8602c9c9b462eaafa1159aa4018859762d
Andrey Parfonov <aparfonov@codenvy.com>
2014-11-13 16:53:58 +0200

    IDEX-1593

--- codenvy-api-runner/src/main/java/com/codenvy/api/runner/internal/ApplicationLogsPublisher.java
+++ codenvy-api-runner/src/main/java/com/codenvy/api/runner/internal/ApplicationLogsPublisher.java
@@ -11,6 +11,7 @@
 package com.codenvy.api.runner.internal;
 
 import com.codenvy.api.core.notification.EventService;
+import com.codenvy.api.core.util.RateExceedDetector;
 
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,13 +22,21 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author andrew00x
  */
 public class ApplicationLogsPublisher extends DelegateApplicationLogger {
-    private final AtomicInteger lineCounter;
-    private final EventService  eventService;
-    private final long          processId;
-    private final String        workspace;
-    private final String        project;
+    private final AtomicInteger      lineCounter;
+    private final EventService       eventService;
+    private final long               processId;
+    private final String             workspace;
+    private final String             project;
+    private final RateExceedDetector maxEventRateChecker;
 
-    public ApplicationLogsPublisher(ApplicationLogger delegate, EventService eventService, long processId, String workspace,
+    private boolean outputEnabled = true;
+
+    int maxLogsRate = 30;
+
+    public ApplicationLogsPublisher(ApplicationLogger delegate,
+                                    EventService eventService,
+                                    long processId,
+                                    String workspace,
                                     String project) {
         super(delegate);
         this.eventService = eventService;
@@ -35,13 +44,28 @@ public class ApplicationLogsPublisher extends DelegateApplicationLogger {
         this.workspace = workspace;
         this.project = project;
         lineCounter = new AtomicInteger(1);
+        maxEventRateChecker = new RateExceedDetector(maxLogsRate);
     }
 
     @Override
     public void writeLine(String line) throws IOException {
-        if (line != null) {
-            eventService.publish(RunnerEvent.messageLoggedEvent(processId, workspace, project,
-                                                                new RunnerEvent.LoggedMessage(line, lineCounter.getAndIncrement())));
+        if (outputEnabled) {
+            double rate;
+            if (maxEventRateChecker.updateAndCheckRate() && (rate = maxEventRateChecker.getRate()) > maxLogsRate) {
+                outputEnabled = false;
+                final String message = String.format(
+                        "[WARNING] Too much output to process.\n" +
+                        "Application has exceeded max allowed rate of output events, detected rate %.2f per second.\n" +
+                        "Application output is disabled.",
+                        rate);
+                eventService.publish(RunnerEvent.messageLoggedEvent(processId, workspace, project,
+                                                                    new RunnerEvent.LoggedMessage(message, lineCounter.getAndIncrement())));
+                return;
+            }
+            if (line != null) {
+                eventService.publish(RunnerEvent.messageLoggedEvent(processId, workspace, project,
+                                                                    new RunnerEvent.LoggedMessage(line, lineCounter.getAndIncrement())));
+            }
         }
         super.writeLine(line);
     }

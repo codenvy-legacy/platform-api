9d35fb84ed0977ecb371f43d6cd98d1980216fc1
Vitaly Parfonov <vparfonov@codenvy.com>
2015-02-05 17:32:21 +0200

    remove not used imports

--- codenvy-api-project/src/test/java/com/codenvy/api/project/server/DtoConverterTest.java
+++ codenvy-api-project/src/test/java/com/codenvy/api/project/server/DtoConverterTest.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.mockito.Matchers.anyMap;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 

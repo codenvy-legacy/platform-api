6e1c072914bccc37fae7c8238783143d6a791f26
Vitaly Parfonov <vparfonov@codenvy.com>
2015-02-24 19:08:26 +0200

    Remove bad code fro git. Keep subversion for a time

--- codenvy-api-project/src/main/java/com/codenvy/api/project/server/ProjectService.java
+++ codenvy-api-project/src/main/java/com/codenvy/api/project/server/ProjectService.java
@@ -882,14 +882,6 @@ public class ProjectService extends Service {
         //TODO: bad solutions add this temporary because don't know how fix it in other way
         //TODO: will be fix soon. Don't remove this code
         //TODO: Vitalii Parfonov
-        if (importer.getId().equals("git") || importer.getId().equals("github")) {
-            ProjectConfig config = project.getConfig();
-            if (!config.getMixinTypes().contains("git")) {
-                config.getMixinTypes().add("git");
-                project.updateConfig(config);
-            }
-        }
-
         if(importer.getId().equals("subversion")){
             ProjectConfig config = project.getConfig();
             if (!config.getMixinTypes().contains("subversion")) {
